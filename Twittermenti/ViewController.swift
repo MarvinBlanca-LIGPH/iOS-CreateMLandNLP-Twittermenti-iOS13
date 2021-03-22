ad19105840d3aba55f0f8e5e8275f17b85dceeb1
MarvinBlanca-LIGPH <76429291+MarvinBlanca-LIGPH@users.noreply.github.com>
2021-03-22 13:14:18 +0800

    Section Done

--- Twittermenti/ViewController.swift
+++ Twittermenti/ViewController.swift
@@ -7,22 +7,79 @@
 //
 
 import UIKit
+import SwifteriOS
+import SwiftyJSON
 
 class ViewController: UIViewController {
     
     @IBOutlet weak var backgroundView: UIView!
     @IBOutlet weak var textField: UITextField!
     @IBOutlet weak var sentimentLabel: UILabel!
-
+    
+    let sentimentClassifier = TweetSentimentClassifier()
+    let swifter = Swifter(consumerKey: "03wPGccPY0qFmEygl55Ce4IJK", consumerSecret: "dozRI93e1IRIlJVfsUso08Fbg9ChYQbgxG2yMzhNTFdANRndkQ")
+    let tweetCount = 100
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
     }
-
+    
     @IBAction func predictPressed(_ sender: Any) {
-    
-    
+        fetchTweet()
     }
     
+    func fetchTweet() {
+        if let searchText = textField.text {
+            swifter.searchTweet(using: searchText,lang: "en",count: tweetCount,tweetMode: .extended, success: { (results, metadata) in
+                
+                var tweets = [TweetSentimentClassifierInput]()
+                for i in 0..<self.tweetCount {
+                    if let tweet = results[i]["full_text"].string {
+                        let tweetForClassification = TweetSentimentClassifierInput(text: tweet)
+                        tweets.append(tweetForClassification)
+                    }
+                }
+                
+                self.calculateScore(with:tweets)
+                
+            }) { (error) in
+                print("There was an error with the Twitter API Request, \(error)")
+            }
+        }
+    }
+    
+    func calculateScore(with tweets: [TweetSentimentClassifierInput]) {
+        do {
+            let predictions = try self.sentimentClassifier.predictions(inputs: tweets)
+            var score = 0
+            for pred in predictions {
+                if pred.label == "Pos" {
+                    score += 1
+                } else if(pred.label == "Neg") {
+                    score -= 1
+                }
+            }
+            updateEmoji(score)
+        } catch {
+            print("Error in making predictions. \(error)")
+        }
+    }
+    
+    func updateEmoji(_ score: Int) {
+        if score > 20 {
+            sentimentLabel.text = "😍"
+        } else if score > 10 {
+            sentimentLabel.text = "😀"
+        } else if score > 0 {
+            sentimentLabel.text = "🙂"
+        } else if score == 0 {
+            sentimentLabel.text = "😐"
+        } else if score > -10 {
+            sentimentLabel.text = "😕"
+        } else if score > -20 {
+            sentimentLabel.text = "😡"
+        } else {
+            sentimentLabel.text = "🤮"
+        }
+    }
 }
-
